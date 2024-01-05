34490d0d789c994193b7180e8400cb1767570f69
Demiladeala <demiladeala@gmail.com>
2024-01-05 03:14:54 +0100

    add properties to backend

--- src/app/api/properties/route.ts
+++ src/app/api/properties/route.ts
@@ -25,24 +25,31 @@ export async function POST(req: NextRequest){
 
     const propertiesResult = await dbClient.property.create({
         data: {
-            images: ["hhttps://crimsonoaks.com.ng/wp-content/uploads/2023/05/7351f264-5075-48b4-b3af-d6fb52bc07ee-1080x570.jpg"],
+            images: ["hhttps://crimsonoaks.com.ng/wp-content/uploads/2023/05/7351f264-5075-48b4-b3af-d6fb52bc07ee-1080x570.jpg", "https://crimsonoaks.com.ng/wp-content/uploads/2023/05/57f62263-0c0b-44e9-870c-6d31a6b01459-1080x570.jpg"],
             name: "Modern Apartment",
-            description: "Executive duplex with an attached office space for a seamless work-life balance.",
-            address: "Executive Lane, Garki",
-            price: 48850000,
+            description: "State-of-the-art apartment complex with multiple units and amenities.",
+            address: "Metropolitan Towers, Gwarinpa",
+            price: 485000000,
             bedrooms: 5,    
             bathrooms: 6,
             sqft: 4000,
-            features:["Home Office", "Smart Home System", "Private Parking"],
-            propertyType: "Detached Duplex",
-            type: "property",
+            features:["Multiple Units", "Swimming Pool", "Fitness Center"],
+            propertyType: "Apartment Complex",
+            type: "land",
             status: "Sale",
             monthlyPayment: 180000,
-            nearbySchools: ["Garki International School"],
-            communicationInfo: "Proximity to government offices and diplomatic zones",
+            nearbySchools: ["Gwarinpa Secondary School"],
+            communicationInfo: "Family-friendly neighborhood with recreational facilities",
         }
     })
 
 
     return NextResponse.json(propertiesResult);
+}
+
+export async function GET(req: NextRequest) {
+
+    const result = await dbClient.property.findMany();
+
+    return NextResponse.json(result);
 }
--- src/app/components/Properties.tsx
+++ src/app/components/Properties.tsx
@@ -1,10 +1,59 @@
 import Link from "next/link"
-import PropertiesCard from "./PropertiesCard"
+import Image from 'next/image'
+import image1 from '../../../public/property-image-1.jpg'
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
 
 
-type Props = {}
+type Props = {
+  id: number;
+  images: string[];
+  name: string;
+  description: string;
+  address: string;
+  price: number;
+  bedrooms: number;
+  bathrooms: number;
+  sqft: number;
+  features: string[];
+  propertyType: string;
+  type: string;
+  status: string;
+  monthlyPayment?: number | null;
+  nearbySchools: string[];
+  communicationInfo: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+const LoadingSkeleton = () => (
+  <div className="border rounded-xl shadow-md hover:shadow-2xl cursor-pointer animate-pulse">
+      <div className="w-full h-[13rem] rounded-xl bg-gray-300"></div>
+      <div className="p-3 pb-18">
+        <div className="font-medium bg-gray-300 py-4 h-5 w-3/4 mb-4"></div>
+      </div>
+  </div>
+);
 
 const Properties = (props: Props) => {
+  const {data, isPending, isError, error } = useQuery({
+    queryKey: ["userProperties"],
+    queryFn: async () => {
+      const { data } = await axios.get("/api/properties")
+      console.log(data)
+      return data
+    },
+  })
+
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+
+  const formatPrice = (price: number) => {
+    return `₦${price.toLocaleString()}`;
+  };
+
   return (
     <>
     <div className='mt-20 md:mt-24'></div>
@@ -14,15 +63,56 @@ const Properties = (props: Props) => {
 
       <div className='my-8 w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 lg:gap-4'>
         {/* LISTINGS */}
+        {
+          isPending ? 
+          <>
+          {Array.from({ length: 8 }, (_, index) => (
+              <LoadingSkeleton key={index} />
+            ))}
+          </> : 
+          <>
+          {data.map((property:Props) => (
+            <div key={property.id} className='border rounded-xl shadow-md hover:shadow-2xl cursor-pointer'>
+              <div className='w-full h-[13rem] rounded-xl'>
+                <Link href={`/ListingDetails/${property.id}`}>
+                    <Image 
+                    className='w-full h-full bg-center bg-no-repeat object-cover rounded-tl-xl rounded-tr-xl'
+                    alt="property"
+                    src={image1}
+                    />
+                </Link>
+              </div>
 
-        <PropertiesCard />
-        <PropertiesCard />
-        <PropertiesCard />
-        <PropertiesCard />
-        <PropertiesCard />
-        <PropertiesCard />
-        <PropertiesCard />
-        <PropertiesCard />
+              <div className='p-3'>
+                <h3 className='font-medium'>{property.name}</h3>
+                {/* <div className="w-full h-px bg-[#eee] my-2"></div> */}
+                <h3 className='font-semibold text-lg'>{formatPrice(property.price)}</h3>
+                <div className='mt-1 flex items-center gap-2 text-sm'>
+                    <div className='flex items-center gap-1'>
+                        <div className='font-semibold'>{property.bedrooms}</div>
+                        <p>Bed</p>
+                    </div>
+                    <div className='flex items-center gap-1'>
+                        <div className='font-semibold'>{property.bathrooms}</div>
+                        <p>Bath</p>
+                    </div>
+                    <div className='flex items-center gap-1'>
+                        <div className='font-semibold'>{property.sqft}</div>
+                        <p>Sqft</p>
+                    </div>
+                </div>
+
+                <div className='mt-2 flex justify-between items-center gap-2'>
+                  <p className='text-sm'>{property.address}</p>
+                  <div>
+                      <button className='px-3 py-1 border border-[#222] rounded-3xl font-semibold text-sm'>Commit</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+          </>
+        }
 
       {/* END LISTINGS */}
       </div>
