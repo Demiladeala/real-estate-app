143903c1f490559b63c3ee6ffd3b34dca6d03942
Demiladeala <demiladeala@gmail.com>
2024-01-10 14:56:43 +0100

    add filter functionality to home page

--- src/app/components/HomeFilter.tsx
+++ src/app/components/HomeFilter.tsx
@@ -1,8 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react'
 import { FiSearch } from 'react-icons/fi'
 
+type PropertyType = {
+    id: number;
+    propertyType: string;
+}
 
 const HomeFilter = () => {
+    const {data, isPending, isError, error } = useQuery({
+        queryKey: ["userProperties"],
+        queryFn: async () => {
+          const { data } = await axios.get("/api/properties")
+          return data
+        },
+      })
+      const uniqueCategories:string[] = Array.from(new Set(data?.map((category: PropertyType) => category.propertyType)));
+
   return (
     <div className="w-[90%] z-10 absolute max-[350px]:left-[1rem] max-[400px]:left-[1.2rem] left-[1.5rem] md:left-[2rem] lg:left-[6rem] bottom-[3.5rem] md:bottom-[10rem] mt-4 md:mt-8 md:w-[80%] py-5 px-2 rounded bg-white/70 flex flex-col md:flex-row-reverse gap-5">
 
@@ -20,14 +35,14 @@ const HomeFilter = () => {
 
         <div className="w-full basis-[100%] md:basis-[50%] flex items-center gap-5">
             <div className="basis-[100%]">
-            <select className="w-full bg-white py-2 outline-none rounded placeholder:text-xs" name="" id="">
+            <select className="w-full bg-white pb-2 outline-none rounded placeholder:text-xs" name="" id="">
                 <option value="">Category</option>
-                <option value="">Flat/Apartment</option>
-                <option value="">Condominium</option>
-                <option value="">Detached Bungalow</option>
-                <option value="">Detached Duplex</option>
-                <option value="">Land</option>
-                <option value="">Commercial Properties</option>
+                {uniqueCategories &&
+                uniqueCategories.map((categoryName: string, index: number):React.ReactNode => (
+                <option key={index} value={categoryName}>
+                  {categoryName}
+                </option>
+              ))}
             </select>
             </div>
         </div>
